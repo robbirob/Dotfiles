66df5bae05333ab1728b32a7f8a0ec27e23e0b63
Robert Freese <robbirob@gmail.com>
2026-05-22 00:46:31 +0200

    feat(hypr): start apps on workspaces

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -46,6 +46,9 @@ hl.on("hyprland.start", function ()
     hl.exec_cmd("hyprpaper")
     hl.exec_cmd("waybar")
     hl.exec_cmd("systemctl --user start hyprpolkitagent")
+    hl.exec_cmd(terminal, { workspace = "1" })
+    hl.exec_cmd("firefox", { workspace = "2" })
+    hl.exec_cmd("hyprctl dispatch workspace 1")
 end)
 
 
